757a02e6a0f9cc9a123a18dd2c042c855dd24c63
Alvar Blomqvist <57719112+Alvarrr@users.noreply.github.com>
2023-02-06 18:36:40 +0200

    Added several methods
    Ability to create mortgage objects and run application

--- src/main/java/alvarblomqvist/Main.java
+++ src/main/java/alvarblomqvist/Main.java
@@ -3,6 +3,6 @@ package alvarblomqvist;
 // Main method for launching application
 public class Main {
     public static void main(String[] args) {
-
+        MortgageCalculator newCalculator = new MortgageCalculator("src/main/prospects.txt");
     }
 }
--- src/main/java/alvarblomqvist/MortgageCalculator.java
+++ src/main/java/alvarblomqvist/MortgageCalculator.java
@@ -9,8 +9,9 @@ import java.io.FileNotFoundException;
 // Class for calculating given mortgage
 public class MortgageCalculator {
 
-    // Reads any given file containing prospects and returns them in an arraylist
-    public ArrayList<String> readProspects(String path) {
+    // Reads any given file containing prospects & returns them in an arraylist
+    public static ArrayList<String> readProspects(String path) {
+
         ArrayList<String> prospectsList = new ArrayList<>();
 
         try {
@@ -30,4 +31,49 @@ public class MortgageCalculator {
         }
         return prospectsList;
     }
+
+    // Creates objects for each mortgage & returns them in an arraylist
+    public ArrayList<Mortgage> createMortgage(ArrayList<String> currentFile) {
+
+        ArrayList<Mortgage> mortgagesList = new ArrayList<>();
+        String[] elements = new String[4];
+
+        for(String i : currentFile) {
+
+            i.replace(",", " ");     // replace commas with whitespace
+            elements = i.split("[\\W]");       // split string at non-letter characters using regex
+
+            // Assign current variables to instance variables in the mortgage
+            String customer = elements[0];
+            float totalLoan = Float.parseFloat(elements[1]);
+            float interest = Float.parseFloat(elements[2]);
+            int years = Integer.parseInt(elements[3]);
+
+            Mortgage mortgage = new Mortgage(customer, totalLoan, interest, years);
+            mortgagesList.add(mortgage);
+
+        }
+        return mortgagesList;
+    }
+
+    // Cleans up commas in the data that interferes with the creation of mortgage objects
+    public String cleanUp(String data) {
+        data = data.replace(",", " ");
+        return data;
+    }
+
+    public MortgageCalculator(String path) {
+        ArrayList<String> prospects = readProspects(path);
+        ArrayList<Mortgage> mortgages = createMortgage(prospects);
+
+        for (Mortgage mortgage : mortgages) {
+            mortgage.calculatePaymentFormula();
+            System.out.println
+                    ("Customer:" + mortgage.getCustomer() + "\n"
+                    + "Total loan:" + mortgage.getTotalLoan() + "\n"
+                    + "Years:" + mortgage.getYears() + "\n"
+                    + "Monthly payment:" + mortgage.getMonthlyPayment());
+        }
+    }
+
 }
